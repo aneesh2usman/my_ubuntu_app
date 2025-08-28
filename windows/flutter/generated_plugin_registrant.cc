f61710a52ff6ed7d7fd7ac14d3b88cf94f922cf9
aneeshenteglow <aneesh@enteglow.net>
2025-08-28 17:46:22 +0530

    Adds Excel export for user data with new dependencies
    Enables exporting user list data to Excel files, integrating new UI options and introducing required dependencies for file saving and permissions. Updates platform plugin registrations and documents new installation steps to support this functionality.

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,18 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <file_saver/file_saver_plugin.h>
 #include <flutter_libserialport/flutter_libserialport_plugin.h>
+#include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileSaverPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSaverPlugin"));
   FlutterLibserialportPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterLibserialportPlugin"));
+  PermissionHandlerWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   Sqlite3FlutterLibsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
 }
